c05389ca6cd27da1a41d426b1f29caf45b898816
Soumajit Choudhury <121515239+soumajit03@users.noreply.github.com>
2025-06-20 15:20:31 +0530

    Update User.js

--- backend/models/User.js
+++ backend/models/User.js
@@ -5,7 +5,7 @@ const userSchema = new mongoose.Schema({
   name:       { type: String, required: true },
   email:      { type: String, required: true, unique: true },
   password:   { type: String, required: true },
-  interests:  [{ type: mongoose.Schema.Types.ObjectId, ref: 'Category' }],
+  interests:  [{ type: mongoose.Schema.Types.ObjectId, ref: 'Category', default: [] }]
 });
 
 // Hash password before saving
